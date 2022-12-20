9a2940fe72e3258307d30c027362d50ddc4d884d
ENGR. MUHAMMAD HAMZA <68488116+HamzaRogers10@users.noreply.github.com>
2022-12-20 22:07:12 +0500

    Changes In the Files

--- cmd/main.go
+++ cmd/main.go
@@ -10,26 +10,25 @@ import (
 	"time"
 )
 
-// main function
 func main() {
 
 	config.ConnectToDb()
 	go backgroundTask()
 
 	r := gin.Default()
+
 	userGroup := r.Group("/api")
 	//register our login information, so it can be verified
 	userGroup.POST("/register", controllers.Register)
 	//This route will be used to authenticate the user by providing  email and password
 	//then generate and gives JSON Web Token in return
 	userGroup.POST("/login", controllers.Login)
-
 	//this will be the route for our protected endpoints
-	protected := r.Group("/api/admin")
-	protected.Use(middleware.JwtAuthMiddleware())
-	protected.GET("/user", controllers.CurrentUser)
+	userGroup.GET("/user", controllers.CurrentUser)
+	userGroup.Use(middleware.JwtAuthMiddleware())
 
 	r.Run(":3000")
+
 }
 
 // Creating a background task function
